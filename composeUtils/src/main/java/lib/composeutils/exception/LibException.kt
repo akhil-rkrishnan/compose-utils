9089b2d0652385becd06123f48d4bdcfc88d82f3
Akhil krishnan <akhil.krishnan@accubits.com>
2022-11-30 17:08:05 +0530

    Added common functionalites Included String extension, Integer Extension Changed version

--- composeUtils/src/main/java/lib/composeutils/exception/LibException.kt
+++ composeUtils/src/main/java/lib/composeutils/exception/LibException.kt
@@ -1,5 +1,5 @@
 package lib.composeutils.exception
 
-sealed class LibException(message: String): Exception(message)
+sealed class IllegalException(message: String): IllegalAccessException(message)
 
-class ColorListViolation(val listSize: Int): LibException("Given list size is $listSize, but expected list size >= 2")
+class ColorListViolation(val listSize: Int): IllegalException("Given list size is $listSize, but expected list size >= 2")
