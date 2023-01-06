bbd66a6e9a8499c820946f6083b29575f76513a1
Akhil krishnan <akhil.krishnan@accubits.com>
2023-01-06 16:29:20 +0530

    Added UiMessage, Modifier extension for blur circle overlay, Replaced timber log with normal log(for time being)

--- composeUtils/src/main/java/lib/composeutils/ui/UiExtensions.kt
+++ composeUtils/src/main/java/lib/composeutils/ui/UiExtensions.kt
@@ -3,8 +3,41 @@ package lib.composeutils.ui
 import android.content.Context
 import android.widget.Toast
 
+/**
+* Show toast with provided message
+ * @param message message to display
+ * @param longToast for long toast
+*/
 fun Context?.showToast(message: String, longToast: Boolean = false) {
     if (this == null || message.isEmpty() || message.isBlank())
         return
     Toast.makeText(this, message, if (longToast) Toast.LENGTH_LONG else Toast.LENGTH_SHORT).show()
+}
+
+/**
+* Show toast with provided message (from string resource / literal string)
+ * @param uiMessage Ui message class for setting string from resource / literal
+ * @param longToast for long toast
+*/
+fun Context.showToast(uiMessage: UiMessage, longToast: Boolean = false) {
+    when (uiMessage) {
+        is UiMessage.FromStringRes -> {
+            Toast.makeText(
+                this,
+                this.getString(uiMessage.res),
+                if (longToast) Toast.LENGTH_LONG else Toast.LENGTH_SHORT
+            ).show()
+        }
+        is UiMessage.FromString -> {
+            showToast(uiMessage.message, longToast)
+        }
+    }
+}
+
+/**
+* Sealed class for Ui message string
+*/
+sealed class UiMessage {
+    data class FromStringRes(val res: Int) : UiMessage()
+    data class FromString(val message: String) : UiMessage()
 }
--- composeUtils/src/main/java/lib/composeutils/utils/LogExtensions.kt
+++ composeUtils/src/main/java/lib/composeutils/utils/LogExtensions.kt
@@ -1,28 +1,28 @@
 package lib.composeutils.utils
 
-import timber.log.Timber
+import android.util.Log
 
-private const val TAG = "LogExtensions"
+private const val TAG = "ComposeUtils"
 fun Any?.loge(tag: String = TAG, filterWord: String = "" ) {
-    Timber.tag(tag).e("%s%s", filterWord, this?.toString())
+    Log.e(tag, "$filterWord ${this?.toString()}" )
 }
 
 fun Any?.logd(tag: String = TAG, filterWord: String = "" ) {
-    Timber.tag(tag).d("%s%s", filterWord, this?.toString())
+    Log.d(tag, "$filterWord ${this?.toString()}")
 }
 
 fun Any?.logi(tag: String = TAG, filterWord: String = "" ) {
-    Timber.tag(tag).i("%s%s", filterWord, this?.toString())
+    Log.i(tag, "$filterWord ${this?.toString()}")
 }
 
 fun Any?.logv(tag: String = TAG, filterWord: String = "" ) {
-    Timber.tag(tag).v("%s%s", filterWord, this?.toString())
+    Log.v(tag, "$filterWord ${this?.toString()}")
 }
 
 fun Any?.logw(tag: String = TAG, filterWord: String = "" ) {
-    Timber.tag(tag).w("%s%s", filterWord, this?.toString())
+    Log.w(tag, "$filterWord ${this?.toString()}")
 }
 
 fun Any?.logwtf(tag: String = TAG, filterWord: String = "" ) {
-    Timber.tag(tag).wtf("%s%s", filterWord, this?.toString())
+    Log.wtf(tag, "$filterWord ${this?.toString()}")
 }
